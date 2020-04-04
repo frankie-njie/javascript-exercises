0438e9b5954b113961104028b207bd1604fe9dfb
frankie <frankie@Frankie-MacBook-Pro.local>
2020-04-04 22:59:25 +0100

    some test

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let splitString = word.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+
+    return joinArray;
 }
 
-module.exports = reverseString
+module.exports = reverseString;
