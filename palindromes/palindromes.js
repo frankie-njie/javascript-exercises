91ced59c1c48704c02705ed9a078025d4eba800c
frankie-njie <njie_frankie@yahoo.com>
2020-05-08 10:46:26 +0100

    functions for palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    finalString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return (finalString.split("").reverse().join("") === finalString);
 }
 
 module.exports = palindromes
