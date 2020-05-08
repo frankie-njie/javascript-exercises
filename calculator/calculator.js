9cd5155b34ad86633c2bb57ce969ec5d89ec3ae4
frankie-njie <njie_frankie@yahoo.com>
2020-05-08 10:36:38 +0100

    functions for calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,37 @@
-function add () {
-	
+function add(a, b) {
+    return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+    return a - b;
 }
 
-function sum () {
-	
+function sum(array) {
+    let count = 0
+    for (let i = 0; i < array.length; i++) {
+        count += array[i];
+    }
+    return count;
 }
 
-function multiply () {
-	
+function multiply(array) {
+    return array.length ? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
+}
+console.log(multiply(2, 4))
+
+function power(a, b) {
+    return Math.pow(a, b);
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(n) {
+    return n ? n * factorial(n - 1) : 1;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
+    add,
+    subtract,
+    sum,
+    multiply,
     power,
-	factorial
+    factorial
 }
